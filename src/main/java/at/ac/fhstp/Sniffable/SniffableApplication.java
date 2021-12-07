a01178bf6a943ab4a379aa58e4805a09b57ae195
Erik Baumann <is191314@fhstp.ac.at>
2021-12-07 20:55:05 +0100

    passt

---
+++ src/main/java/at/ac/fhstp/Sniffable/SniffableApplication.java
@@ -0,0 +1,13 @@
+package at.ac.fhstp.Sniffable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SniffableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SniffableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/ac/fhstp/Sniffable/SniffableApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.fhstp.Sniffable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SniffableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
